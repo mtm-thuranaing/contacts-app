d9a4602f2a1c7b4ccb4df1f8374ba48773a2d2ec
ThuraNaing <scm.thuranang@gmail.com>
2020-10-26 12:45:05 +0630

    added bootstrap js and nav header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+import { ContactComponent } from './pages/contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: ContactComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '/contact', pathMatch: 'full' },
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
