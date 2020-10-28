062c7353b47992ec5f68f5a81502cfed4ef18ce6
ThuraNaing <scm.thuranang@gmail.com>
2020-10-28 12:08:28 +0630

    fixed mail validation at edit page

--- src/app/pages/contact-edit/contact-edit.component.ts
+++ src/app/pages/contact-edit/contact-edit.component.ts
@@ -26,7 +26,7 @@ export class ContactEditComponent implements OnInit {
     this.editForm = new FormGroup({
       id: new FormControl(''),
       name: new FormControl('', [Validators.required]),
-      email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
+      email: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]),
       phone: new FormControl('', [Validators.required, Validators.minLength(11), Validators.pattern('^((\\+95-?)|0)?[0-9]{10}$')])
     });
   }
